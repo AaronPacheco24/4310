0e4d81846bbc97b17a477b4024c27b5d91e25c20
Your Name <aaronpacheco87@gmail.com>
2022-11-10 01:08:16 -0800

    Find version number using: uname - a ll /usr/src

---
+++ test_mod00.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/kern_levels.h>
+
+MODULE_LICENSE("GPL");
+
+int ece_init(void)
+{
+    printk(KERN_INFO " ECE4310: Start here\n ");
+    return 0;
+}
+void ece_end(void)
+{
+    printk(KERN_INFO " ECE 4310: End Here\n");
+}
+
+module_init(ece_init);
+module_exit(ece_end);
