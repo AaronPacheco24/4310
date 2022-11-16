f1291707ac003a9559e75db133e3bfeb41f6ec67
AaronPacheco-Laptop <aaronpacheco87@gmail.com>
2022-11-15 16:37:26 -0800

    capitlization adjustments

--- KernelModule/fileoperations.c
+++ KernelModule/fileoperations.c
@@ -5,12 +5,10 @@ struct file_operations {
 	 ssize_t(*read) (struct file *, char __user *, size_t, loff_t *);
 	 ssize_t(*aio_read) (struct kiocb *, char __user *, size_t, loff_t);
 	 ssize_t(*write) (struct file *, const char __user *, size_t, loff_t *);
-	 ssize_t(*aio_write) (struct kiocb *, const char __user *, size_t,
-			      loff_t);
+	 ssize_t(*aio_write) (struct kiocb *, const char __user *, size_t,loff_t);
 	int (*readdir) (struct file *, void *, filldir_t);
 	unsigned int (*poll) (struct file *, struct poll_table_struct *);
-	int (*ioctl) (struct inode *, struct file *, unsigned int,
-		      unsigned long);
+	int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
 	int (*mmap) (struct file *, struct vm_area_struct *);
 	int (*open) (struct inode *, struct file *);
 	int (*flush) (struct file *);
@@ -19,16 +17,10 @@ struct file_operations {
 	int (*aio_fsync) (struct kiocb *, int datasync);
 	int (*fasync) (int, struct file *, int);
 	int (*lock) (struct file *, int, struct file_lock *);
-	 ssize_t(*readv) (struct file *, const struct iovec *, unsigned long,
-			  loff_t *);
-	 ssize_t(*writev) (struct file *, const struct iovec *, unsigned long,
-			   loff_t *);
-	 ssize_t(*sendfile) (struct file *, loff_t *, size_t, read_actor_t,
-			     void __user *);
-	 ssize_t(*sendpage) (struct file *, struct page *, int, size_t,
-			     loff_t *, int);
-	unsigned long (*get_unmapped_area) (struct file *, unsigned long,
-					    unsigned long, unsigned long,
-					    unsigned long);
+	 ssize_t(*readv) (struct file *, const struct iovec *, unsigned long, loff_t *);
+	 ssize_t(*writev) (struct file *, const struct iovec *, unsigned long,loff_t *);
+	 ssize_t(*sendfile) (struct file *, loff_t *, size_t, read_actor_t,void __user *);
+	 ssize_t(*sendpage) (struct file *, struct page *, int, size_t,loff_t *, int);
+	unsigned long (*get_unmapped_area) (struct file *, unsigned long, unsigned long, unsigned long,unsigned long);
 };
 	
--- KernelModule/testmod00.make
+++ KernelModule/testmod00.make
@@ -5,8 +5,8 @@ KDIR=/usr/src/linux-headers-5.11.0-38-generic
 CFLAGS = -D__KERNEL__ -DMODULE = I$(KDIR)/include -Wall
 
 all:
-    $(make) -C $(KDIR) M=$(shell pwd) $(KCONFIG) modules
+    $(MAKE) -C $(KDIR) M=$(shell pwd) $(KCONFIG) modules
 
 clean:
-    $(make) -C $(KDIR) M=$(shell pwd) clean
+    $(MAKE) -C $(KDIR) M=$(shell pwd) clean
     rm -f *.o
