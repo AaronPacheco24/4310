8c9f670a1eae155535f01491f7ca636340875b51
AaronPacheco-Laptop <aaronpacheco87@gmail.com>
2022-11-15 17:01:07 -0800

    adjusted linux version on the src line

--- KernelModule/test_mod00.c
+++ KernelModule/test_mod00.c
@@ -6,12 +6,12 @@ MODULE_LICENSE("GPL");
 
 int ece_init(void)
 {
-    printk(KERN_INFO " ECE4310: Start here\n ");
+    printk(KERN_INFO " ECE4310: Start here\n "); //print kernel | kern info is an interger. its a flag. defined in kern levels header. between 0-7.  based on that level the system will decide if it wants to keep track of your message or not.
     return 0;
 }
 void ece_end(void)
 {
-    printk(KERN_INFO " ECE 4310: End Here\n");
+    printk(KERN_INFO " ECE 4310: End Here\n"); //prints in the log file.
 }
 
 module_init(ece_init);
