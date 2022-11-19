447633f3f6009730d786896dd4fff81e4709e271
Aaron P <117862685+AaronPacheco24@users.noreply.github.com>
2022-11-19 00:43:14 -0800

    Forking example
    This an example of forking. Make sure to use #include <unistd.h>

--- forkingEx1.c
+++ forkingEx1.c
@@ -7,7 +7,7 @@ int main()
 {
     int subproc_ID = fork();//create the fork
     if ( subproc_ID == 0)
-    {
+    { 
         printf("\nThis is child process\n"); //print that we are proceeding in child subprocess
         int wait_return = wait(NULL); // Get the return value for the wait
         printf("\n The wait() in the child returned: %d\n", wait_return); //The value is -1. man wait states this is to be expected due to it waiting for a child
